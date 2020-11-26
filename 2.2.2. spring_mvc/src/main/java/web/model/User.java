3414933c25128ef79fab078610b5f8386bf36b8d
Igor Pavlenkov <pavlenkov.igor@mail.ru>
2020-11-26 23:12:12 +0300

    2.3.1

---
+++ 2.2.2. spring_mvc/src/main/java/web/model/User.java
@@ -0,0 +1,62 @@
+package web.model;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name ="SERNAME")
+    private String sername;
+
+    @Column(name = "AGE")
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSername() {
+        return sername;
+    }
+
+    public void setSername(String sername) {
+        this.sername = sername;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", sername='" + sername + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
